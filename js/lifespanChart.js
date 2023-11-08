bc2e527162f075947c7e1053d43ae257495f405d
SherlockT321 <thsherlock@ctemc.org>
2023-11-08 10:29:41 -0500

    Added result charts and data

---
+++ js/lifespanChart.js
@@ -0,0 +1,130 @@
+
+async function getData() {
+    const response = await fetch('../data/caterpillar-lifespan.csv'); // .. moves up 1 folder
+    const data = await response.text();    // CSV is in TEXT format
+    //console.log(data);
+
+    const xDate = [];  //x-axis labels = day values
+    const yWTLSpan = [];  //y-axis WTL lifespan values
+    const yDLSpan = []; //y-axis DL lifespan values 
+    const yCTLSpan = []; //y-axis CTL lifespan values
+
+    // \n - new line character
+    // split('\n') will separate the table into an array of individual rows 
+    // slice(start, end) - return a new array starting at index start 
+    //      up to but not including index end. 
+    const table = data.split('\n').slice(1);
+    //console.log(table);
+
+    table.forEach(row => {
+        const columns = row.split(','); // split each row on the commas
+        const date = columns[0];        // assign a day value 
+        xDate.push(date);              // push day values into day array
+        
+        const wtlSpan = parseFloat(columns[1]);    //assign warm temp lifespan values 
+        yWTLSpan.push(wtlSpan);         // push lifespan values to store mean lifespan values 
+
+        const dlSpan = parseFloat(columns[2]);      // daylight lifespan deviation values
+        yDLSpan.push(dlSpan);
+        
+        const ctlSpan = parseFloat(columns[3]);      // cool temp light deviation values
+        yCTLSpan.push(ctlSpan);
+
+        //console.log(xDate, yWTLSpan, yDLSpan, yCTLSpan);
+    });
+    return{xDate, yWTLSpan, yDLSpan, yCTLSpan};
+};
+
+async function createChart() {
+    const data = await getData();   // createChart will wait until getData() is finished processing
+    const ctx = document.getElementById('lifespanChart');
+    const lifespanChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: data.xDate,
+            datasets: [
+                {
+                    label: 'The Number of Caterpillars/Butteryflies under the warm temp. light alive',
+                    data: data.yWTLSpan, 
+                    fill: false,
+                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                    borderColor: 'rgba(255, 99, 132, 1)',
+                    borderWidth: 1
+                },
+                {
+                    label: 'The Number of Caterpillars/Butteryflies under the daylight temp. LED alive',
+                    data: data.yDLSpan, 
+                    fill: false,
+                    backgroundColor: 'rgba(0, 102, 255, 0.2)',
+                    borderColor: 'rgba(0, 102, 255, 1)',
+                    borderWidth: 1
+                },
+                {
+                    label: 'The Number of Caterpillars/Butteryflies under the cool temp. light alive',
+                    data: data.yCTLSpan, 
+                    fill: false,
+                    backgroundColor: 'rgba(0, 153, 51, 0.2)',
+                    borderColor: 'rgba(0, 153, 51, 1)',
+                    borderWidth: 1
+                }
+            ]
+        },
+        options: {
+            responsive: true,   // Re-size based on screen size
+            scales: {           // Display options for x & y axes
+                x: {
+                    title: {
+                        display: true,
+                        text: 'Date',   // x-axis title
+                        font: {         // font properties
+                            size: 20
+                        }
+                    },
+                    ticks: {
+                        callback: function(val, index) {
+                            // Labeling of tick marks can be controlled by code and font size
+                            return index % 3 === 0 ? this.getLabelForValue(val) : '';
+                        },
+                        font: {
+                            size: 16
+                        }
+                    }
+                },
+                y: {
+                    title: {
+                        display: true,
+                        text: '# Caterpillars Alive',
+                        font: {
+                            size: 20
+                        }
+                    },
+                    ticks: {
+                        maxTicksLimit: data.yWTLSpan.length/2,    //limit # of ticks
+                        font: {
+                            size: 12
+                        }
+                    }
+                }
+            },
+            plugins: {      //Display options
+                title: {
+                    display: true,
+                    text: 'The Number of Caterpillars Alive Over Time (11/7/22 - 12/25/22)',
+                    font: {
+                        size: 24
+                    },
+                    padding: {
+                        top: 10,
+                        bottom: 30
+                    }
+                },
+                legend: {
+                    align: 'start',
+                    position: 'bottom'
+                }
+            }
+        }
+    });
+};
+
+createChart();
---
+++ js/massChart.js
@@ -0,0 +1,130 @@
+
+async function getData() {
+    const response = await fetch('../data/caterpillar-mean-mass.csv'); // .. moves up 1 folder
+    const data = await response.text();    // CSV is in TEXT format
+    //console.log(data);
+
+    const xDate = [];  //x-axis labels = day values
+    const yWTLMass = [];  //y-axis WTL mass values
+    const yDLMass = []; //y-axis DL mass values 
+    const yCTLMass = []; //y-axis CTL mass values
+
+    // \n - new line character
+    // split('\n') will separate the table into an array of individual rows 
+    // slice(start, end) - return a new array starting at index start 
+    //      up to but not including index end. 
+    const table = data.split('\n').slice(1);
+    //console.log(table);
+
+    table.forEach(row => {
+        const columns = row.split(','); // split each row on the commas
+        const date = columns[0];        // assign a day value 
+        xDate.push(date);              // push day values into day array
+        
+        const wtlMass = parseFloat(columns[1]);    //assign warm temp mass values 
+        yWTLMass.push(wtlMass);         // push mass values to store mean mass values 
+
+        const dlMass = parseFloat(columns[2]);      // daylight mass deviation values
+        yDLMass.push(dlMass);
+        
+        const ctlMass = parseFloat(columns[3]);      // cool temp light deviation values
+        yCTLMass.push(ctlMass);
+
+        //console.log(xDate, yWTLMass, yDLMass, yCTLMass);
+    });
+    return{xDate, yWTLMass, yDLMass, yCTLMass};
+};
+
+async function createChart() {
+    const data = await getData();   // createChart will wait until getData() is finished processing
+    const ctx = document.getElementById('massChart');
+    const massChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: data.xDate,
+            datasets: [
+                {
+                    label: 'Average Mass of One Caterpillar/Butteryfly under the warm temp. light in grams',
+                    data: data.yWTLMass, 
+                    fill: false,
+                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                    borderColor: 'rgba(255, 99, 132, 1)',
+                    borderWidth: 1
+                },
+                {
+                    label: 'Average Mass of One Caterpillar/Butteryfly under the daylight temp. LED in grams',
+                    data: data.yDLMass, 
+                    fill: false,
+                    backgroundColor: 'rgba(0, 102, 255, 0.2)',
+                    borderColor: 'rgba(0, 102, 255, 1)',
+                    borderWidth: 1
+                },
+                {
+                    label: 'Average Mass of One Caterpillar/Butteryfly under the cool temp. light in grams',
+                    data: data.yCTLMass, 
+                    fill: false,
+                    backgroundColor: 'rgba(0, 153, 51, 0.2)',
+                    borderColor: 'rgba(0, 153, 51, 1)',
+                    borderWidth: 1
+                }
+            ]
+        },
+        options: {
+            responsive: true,   // Re-size based on screen size
+            scales: {           // Display options for x & y axes
+                x: {
+                    title: {
+                        display: true,
+                        text: 'Date',   // x-axis title
+                        font: {         // font properties
+                            size: 20
+                        }
+                    },
+                    ticks: {
+                        callback: function(val, index) {
+                            // Labeling of tick marks can be controlled by code and font size
+                            return this.getLabelForValue(val);
+                        },
+                        font: {
+                            size: 16
+                        }
+                    }
+                },
+                y: {
+                    title: {
+                        display: true,
+                        text: 'Average Mass (g)',
+                        font: {
+                            size: 20
+                        }
+                    },
+                    ticks: {
+                        maxTicksLimit: data.yWTLMass.length,    //limit # of ticks
+                        font: {
+                            size: 12
+                        }
+                    }
+                }
+            },
+            plugins: {      //Display options
+                title: {
+                    display: true,
+                    text: 'Average Mass of One Caterpillar/Butterfly Over Time (11/7/22 - 12/5/22)',
+                    font: {
+                        size: 24
+                    },
+                    padding: {
+                        top: 10,
+                        bottom: 30
+                    }
+                },
+                legend: {
+                    align: 'start',
+                    position: 'bottom'
+                }
+            }
+        }
+    });
+};
+
+createChart();
